852f676af0cb39c8b92f8b6c159ddbc26bb3567a
Robbie Cook <robbieck7@gmail.com>
2020-10-11 16:13:18 +1300

    Filling out core sections

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Welcome to the international developer community!</h1>
+      <section>
+        <h2></h2>
+      </section>
+      <section>
+        <h2>Organisations</h2>
+        <div>
+          <a href="nz.developercommunity.org">
+            New Zealand Developer Community
+          </a>
+          <a href="international.developercommunity.org">
+            International Developer Community
+          </a>
+        </div>
+      </section>
+      <section>
+        <h2>Newsletter</h2>
+        <div>
+          <p>Please sign up to get access to our newsletter</p>
+        </div>
+      </section>
+    </main>
   );
 }
 
