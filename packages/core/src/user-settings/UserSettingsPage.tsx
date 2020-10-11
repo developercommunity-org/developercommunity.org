d7e40ec2941005d5187086900fd99bf021ca6ac9
Robbie Cook <robbieck7@gmail.com>
2020-10-12 00:50:02 +1300

    Fixed routing

--- packages/core/src/user-settings/UserSettingsPage.tsx
+++ packages/core/src/user-settings/UserSettingsPage.tsx
@@ -1,19 +1,27 @@
-  /** @jsx jsx */
-  import { css, jsx } from "@emotion/core"
-  import React from "react"
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import firebase from "firebase";
+import React from "react";
+import { useParams } from "react-router-dom";
 
-  /**
-   * Interface for UserSettingsPage props
-   */
-  export interface UserSettingsPageProps {
-    children?: any;
-  };
+/**
+ * Interface for UserSettingsPage props
+ */
+export interface UserSettingsPageProps {
+  children?: any;
+}
 
-  /**
-   *  A UserSettingsPage component.
-   */
-  const UserSettingsPage: React.FC<UserSettingsPageProps> = (props) => {
-    return <div css={css``}>UserSettingsPage component</div>
-  }
+/**
+ *  A UserSettingsPage component.
+ */
+const UserSettingsPage: React.FC<UserSettingsPageProps> = (props) => {
+  // let { uid } = useParams();
 
-  export default UserSettingsPage
+  return (
+    <div css={css``}>
+      UserSettingsPage component {firebase.auth().currentUser?.displayName}
+    </div>
+  );
+};
+
+export default UserSettingsPage;
